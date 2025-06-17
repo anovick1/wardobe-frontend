02ea02bb3874ee42c6d544f676e303f7abcc41db
anovick1 <averynov@yahoo.com>
2025-06-17 16:05:16 -0700

    bug fix

--- components/wardrobe/AddNewModal.jsx
+++ components/wardrobe/AddNewModal.jsx
@@ -43,7 +43,7 @@ export default function AddNewModal({
 
   const handleLinkUpload = () => {
     onClose();
-    navigation.navigate("AddLink");
+    navigation.navigate("WebView");
   };
 
   return (
